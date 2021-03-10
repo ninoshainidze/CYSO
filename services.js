54a8697333ee1fb97f819b17e24ee7e66a1af413
ninoshainidze <ninoshainidze>
2021-03-11 01:07:52 +0400

    .

---
+++ services.js
@@ -0,0 +1,40 @@
+function myFunction1() {
+	let x = document.getElementById("mydiv1");
+	if (x.style.display == "none") {
+  		document.getElementById("mydiv1").style.display = "block";
+	}
+	else{
+		document.getElementById("mydiv1").style.display = "none";
+	}
+}
+
+function myFunction2(){
+	let x = document.getElementById("mydiv2");
+	if (x.style.display == "none") {
+  		document.getElementById("mydiv2").style.display = "block";
+	}
+	else{
+		document.getElementById("mydiv2").style.display = "none";
+	}
+} 
+
+function myFunction3(){
+	let x = document.getElementById("mydiv3");
+	if (x.style.display == "none") {
+  		document.getElementById("mydiv3").style.display = "block";
+	}
+	else{
+		document.getElementById("mydiv3").style.display = "none";
+	}
+}
+
+function myFunction4(){
+	let x = document.getElementById("mydiv4");
+	if (x.style.display == "none") {
+  		document.getElementById("mydiv4").style.display = "block";
+	}
+	else{
+		document.getElementById("mydiv4").style.display = "none";
+	}
+}
+
